e978e1dfdd8aa964e2cda4fc788a4b6bd410e3ec
xypp <2952795729@qq.com>
2026-03-17 19:32:49 +0800

    实现了自动图片AB创建（基于模板AB修改法） 优化部分逻辑

--- Program.cs
+++ Program.cs
@@ -25,45 +25,55 @@ namespace ModLoader
             ProcessMods();
             Save();
         }
+        static void StartGame()
+        {
+            if (executable == "") return;
 
+        }
         static void ProcessMods()
         {
-            string modsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "mods");
+            string modsDirectory = Path.Combine(basePath, "mods");
 
             if (!Directory.Exists(modsDirectory))
             {
                 Directory.CreateDirectory(modsDirectory);
             }
-            foreach (string modDir in Directory.GetDirectories(modsDirectory))
-            {
-                if (File.Exists(Path.Combine(modDir, "replace.txt")))
-                {
-                    Console.WriteLine($"Processing {modDir}");
-                    ApplyMod(modDir);
-                }
-            }
+            ApplyMod(modsDirectory);
         }
         static ContentCatalogData ccd;
+        static string basePath = "";
         static string savePath = "";
         static string executable = "";
+        static string referenceBundle = "";
         static bool skip = false;
         static Dictionary<String,ResourceLocation> generatedAbDict = new Dictionary<String,ResourceLocation>();
         static void Init()
         {
             string localPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData", "LocalLow"); 
             string currentPath = Directory.GetCurrentDirectory();
-            string[] allSubDirs = Directory.GetDirectories(currentPath);
             string appName = "";
-            foreach (string subDir in allSubDirs){
-                if (subDir.EndsWith("_Data")){
-                    appName = subDir.Substring(0,subDir.Length - 5);
+            for (int i = 0; i < 2; i++)
+            {
+                string[] allSubDirs = Directory.GetDirectories(currentPath);
+                foreach (string subDir in allSubDirs)
+                {
+                    if (subDir.EndsWith("_Data"))
+                    {
+                        appName = subDir.Substring(0, subDir.Length - 5);
+                    }
                 }
+                if(appName != "")
+                {
+                    break;
+                }
+                currentPath = Directory.GetParent(currentPath).FullName;
             }
             if(appName == "")
             {
-                Console.Error.WriteLine("在游戏运行目录下执行该软件");
+                Console.Error.WriteLine("在游戏运行目录下安装该软件");
                 return;
             }
+            basePath = currentPath;
             executable = Path.Combine(currentPath, appName + ".exe");
             Console.WriteLine($"使用 {executable} 作为可执行文件");
             string[] appData = File.ReadAllLines(Path.Combine(currentPath, appName+"_Data","app.info"));
@@ -73,6 +83,9 @@ namespace ModLoader
             }
             Console.WriteLine($"{appData[0]} / {appData[1]} ");
             string presistDir = Path.Combine(localPath, appData[0], appData[1], "com.unity.addressables");
+            referenceBundle = Path.Combine(currentPath,"ref.bundle");
+
+
             string addressableSettings = File.ReadAllText(Path.Combine(currentPath, appName + "_Data", "StreamingAssets", "aa", "settings.json"));
             int offset1 = addressableSettings.IndexOf("/catalog_")+9;
             int offset2 = addressableSettings.IndexOf(".hash",offset1);
@@ -83,7 +96,7 @@ namespace ModLoader
             string lastHash = "";
             if(Path.Exists(Path.Combine(presistDir, "catalog_" + version + ".hash_modded")))
             {
-                lastHash = File.ReadAllText(File.ReadAllText(Path.Combine(presistDir, "catalog_" + version + ".hash_modded")));
+                lastHash = File.ReadAllText(Path.Combine(presistDir, "catalog_" + version + ".hash_modded"));
             }
             savePath = Path.Combine(presistDir, "catalog_" + version + ".json");
             if (!Path.Exists(savePath))
@@ -105,72 +118,100 @@ namespace ModLoader
         }
         static void ApplyMod(string modPath)
         {
-            string[] files = File.ReadAllLines(Path.Combine(modPath,"replace.txt"));
-            foreach (string file in files)
+            if (File.Exists(Path.Combine(modPath, "replace.txt")))
             {
-                if (file.Trim().StartsWith("#"))
-                    continue;
-                string[] def = file.Split(':');
-                if (def.Length < 2) 
-                    continue;
-                string name = def[0];
-                string bundle= def[1];
-                string bundleFile = Path.Combine(modPath, bundle);
-                if (!Path.Exists(bundleFile))
+                string[] files = File.ReadAllLines(Path.Combine(modPath, "replace.txt"));
+                foreach (string file in files)
                 {
-                    Console.WriteLine($"[W] {bundleFile} not exist");
+                    if (file.Trim().StartsWith("#"))
+                        continue;
+                    string[] def = file.Split(':');
+                    if (def.Length < 2)
+                        continue;
+                    string name = def[0];
+                    string bundle = def[1];
+                    string bundleFile = Path.Combine(modPath, bundle);
+                    ApplyBundleMod(name, bundleFile);
+                }
+            }
+            foreach(var file in Directory.GetFiles(modPath))
+            {
+                if (Path.GetExtension(file).ToLower() == ".png" || Path.GetExtension(file).ToLower() == ".jpg")
+                {
+                    string bundle = AB.createImageAbSingle(file);
+                    ApplyBundleMod(Path.GetFileNameWithoutExtension(file), bundle,"d");
+                }
+            }
+            foreach(var dir in Directory.GetDirectories(modPath))
+            {
+                if(dir != "." && dir != "..")
+                {
+                    ApplyMod(Path.Combine(modPath,dir));
+                }
+            }
+        }
+        static void ApplyBundleMod(string name,string bundleFile,string containerRedir = "")
+        {
+            if (!Path.Exists(bundleFile))
+            {
+                Console.WriteLine($"[W] {bundleFile} not exist");
+                return;
+            }
+
+            if (!ccd.Resources.ContainsKey(name))
+            {
+                Console.WriteLine($"[W] {name} not found in addressable");
+                return;
+            }
+
+            foreach (var location in ccd.Resources[name])
+            {
+                if (location.ProviderId != "UnityEngine.ResourceManagement.ResourceProviders.BundledAssetProvider")
+                {
+                    Console.WriteLine($"[W] When processing {name}");
+                    Console.WriteLine($"[W] Unsupported provider type {location.ProviderId}");
+                    continue;
+                }
+                ResourceLocation? firstDep = null;
+                if (location.Dependencies != null)
+                {
+                    firstDep = location.Dependencies.First();
+                }
+                else if (location.DependencyKey != null)
+                {
+                    firstDep = ccd.Resources[location.DependencyKey].First();
+                }
+                if (firstDep == null)
+                {
+                    Console.WriteLine($"[W] When processing {name}");
+                    Console.WriteLine($"[W] No Dep Found");
+                    continue;
+                }
+                var rl = getAbIdFor(bundleFile, firstDep);
+
+                if (rl == null)
+                {
+                    Console.WriteLine($"[W] When processing {name}");
+                    Console.WriteLine($"[W] Fail creating dummy Bundle");
                     continue;
                 }
 
-                if (!ccd.Resources.ContainsKey(name))
+
+                if (location.Dependencies != null)
                 {
-                    Console.WriteLine($"[W] {name} not found in addressable");
-                    continue;
+                    location.Dependencies.Clear();
+                    location.Dependencies.Add(rl);
+                }
+                else if (location.DependencyKey != null)
+                {
+                    location.DependencyKey = rl.PrimaryKey;
                 }
 
-                foreach (var location in ccd.Resources[name])
+                if(containerRedir != "")
                 {
-                    if (location.ProviderId != "UnityEngine.ResourceManagement.ResourceProviders.BundledAssetProvider")
-                    {
-                        Console.WriteLine($"[W] When processing {name}");
-                        Console.WriteLine($"[W] Unsupported provider type {location.ProviderId}"); 
-                        continue;
-                    }
-                    ResourceLocation? firstDep = null;
-                    if (location.Dependencies != null)
-                    {
-                        firstDep = location.Dependencies.First();
-                    }
-                    else if (location.DependencyKey != null)
-                    {
-                        firstDep = ccd.Resources[location.DependencyKey].First();
-                    }
-                    if (firstDep == null)
-                    {
-                        Console.WriteLine($"[W] When processing {name}");
-                        Console.WriteLine($"[W] No Dep Found");
-                        continue;
-                    }
-                    var rl = getAbIdFor(bundleFile, firstDep);
-
-                    if (rl== null)
-                    {
-                        Console.WriteLine($"[W] When processing {name}");
-                        Console.WriteLine($"[W] Fail creating dummy Bundle");
-                        continue;
-                    }
-
-
-                    if (location.Dependencies != null)
-                    {
-                        location.Dependencies.Clear();
-                        location.Dependencies.Add(rl);
-                    }
-                    else if (location.DependencyKey != null)
-                    {
-                        location.DependencyKey = rl.PrimaryKey;
-                    }
+                    location.InternalId = containerRedir; 
                 }
+                Console.WriteLine($"Resource {name} --> {rl.PrimaryKey}");
             }
         }
         static ResourceLocation? getAbIdFor(string path, ResourceLocation reference)
